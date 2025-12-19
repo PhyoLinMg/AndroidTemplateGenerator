39d593f8ee1bdc2d95da3af49bb87a678848a060
PhyoLinMg <gangsterplm@gmail.com>
2025-12-19 21:36:00 +0700

    update to graalvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
-	//id("org.graalvm.buildtools.native") version "0.10.1"
+	id("org.graalvm.buildtools.native") version "0.10.1"
 }
 
 group = "dev.linmaung"
@@ -43,20 +43,20 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-//graalvmNative {
-//	binaries {
-//		named("main") {
-//			imageName.set("app")
-//			mainClass.set("dev.linmaung.androidtemplategenerator.AndroidtemplategeneratorApplicationKt")
-//			resources.autodetect()
-//			buildArgs.addAll(
-//				listOf(
-//					"--initialize-at-run-time=io.netty.channel.epoll",
-//					"--initialize-at-run-time=io.netty.channel.unix",
-//					"--initialize-at-run-time=io.netty.resolver.dns",
-//					"-H:+ReportExceptionStackTraces"
-//				)
-//			)
-//		}
-//	}
-//}
+graalvmNative {
+	binaries {
+		named("main") {
+			imageName.set("app")
+			mainClass.set("dev.linmaung.androidtemplategenerator.AndroidtemplategeneratorApplicationKt")
+			resources.autodetect()
+			buildArgs.addAll(
+				listOf(
+					"--initialize-at-run-time=io.netty.channel.epoll",
+					"--initialize-at-run-time=io.netty.channel.unix",
+					"--initialize-at-run-time=io.netty.resolver.dns",
+					"-H:+ReportExceptionStackTraces"
+				)
+			)
+		}
+	}
+}
